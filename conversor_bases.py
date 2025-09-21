e96d16dae5dfe1f8be9e835bb1ec0739790974b0
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2025-09-21 20:35:48 -0300

    Update conversor_bases.py

--- conversor_bases.py
+++ conversor_bases.py
@@ -12,17 +12,17 @@ print('-------------------------------------------------------------------------
 
 print('Preenchimento dos números')
 numero_base_1 = input('Digite o numero: ')
-base_1 = int(input('Digite a base do numero inserido: '))
+base_origem = int(input('Digite a base do numero inserido: '))
 
-while not bases.count(base_1):
-    base_1 = int(input('A base que você inseriu não está contida no escopo deste programa! Tente novamente!\nAs bases disponíveis são de 2 a 26: '))
+while not bases.count(base_origem):
+    base_origem = int(input('A base que você inseriu não está contida no escopo deste programa! Tente novamente!\nAs bases disponíveis são de 2 a 26: '))
 
-base_2 =  int(input('Digite a base a qual você deseja converter: '))
+base_destino =  int(input('Digite a base a qual você deseja converter: '))
 
-while not bases.count(base_2):
-    base_2 = int(input('A base que você inseriu não está contida no escopo deste programa! Tente novamente!\nAs bases disponíveis são de 2 a 26: '))
+while not bases.count(base_destino):
+    base_destino = int(input('A base que você inseriu não está contida no escopo deste programa! Tente novamente!\nAs bases disponíveis são de 2 a 26: '))
 
-if base_1 == base_2:
+if base_origem == base_destino:
     print(f'As bases são iguais, assim, o número será o mesmo: {numero_base_1}')
     exit()
   
@@ -35,15 +35,15 @@ numero_em_polinomio = ''
 
 for i in range(len(numero_base_1)):
     if len(numero_base_1) - (i+1):
-        numero_em_polinomio += f'({numero_base_1[i]} * {base_1} ** {len(numero_base_1) - (i+1)}) + '
+        numero_em_polinomio += f'{numero_base_1[i]}*{base_origem}**{len(numero_base_1) - (i+1)} + '
         
     else:
-        numero_em_polinomio += f'({numero_base_1[i]} * {base_1} ** {len(numero_base_1) - (i+1)})'
+        numero_em_polinomio += f'{numero_base_1[i]} * {base_origem} ** {len(numero_base_1) - (i+1)}'
     n += 1
 
-print(f'Escrevendo o numero na base {base_1} como polinômio: {numero_em_polinomio}\n')
+print(f'Escrevendo o numero na base {base_origem} como polinômio: {numero_em_polinomio}\n')
 
 print('-------------------------------------------------------------------------------------------\n')
 
-print(f'Fatorando os termos do polinômio: {numero_em_polinomio}\n')
-
+print(f'Transformando cada termo de {numero_em_polinomio} para a base de destino {base_destino}\n')
+print(base_n.transforma_polinomio(numero_em_polinomio, base_destino))
