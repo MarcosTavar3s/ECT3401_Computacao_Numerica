cd05439b810720f4732667a72099a96ebd46a4a1
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2025-09-26 12:43:11 -0300

    Update conversor_bases.py

--- conversor_de_bases/conversor_bases.py
+++ conversor_de_bases/conversor_bases.py
@@ -13,107 +13,117 @@ print('-------------------------------------------------------------------------
 print('Esse programa foi desenvolvido por Marcos Aurélio para a disciplina de Computação Numérica!\n')
 print('-------------------------------------------------------------------------------------------\n')
 
-print('O que voce deseja fazer? Digite o número correspondente para a operação')
-print('-------------------------------------------------------------------------------------------\n')
-print('1 - Conversão entre bases')
-print('2 - Soma em um base n')
-print('3 - Multiplicação em uma base n')
-print('OBS: Bases disponíveis vão de 2 até 26 (P)')
-print('-------------------------------------------------------------------------------------------')
+continua = 's'
 
-operacao = int(input("Digite a operação escolhida: "))
-
-if operacao == 1:
-    print('\n-------------------------------------------------------------------------------------------\n')
-    print('Conversão de bases selecionada com sucesso\n')
+while continua.lower() == "s":
+    print('O que voce deseja fazer? Digite o número correspondente para a operação')
     print('-------------------------------------------------------------------------------------------\n')
+    print('1 - Conversão entre bases')
+    print('2 - Soma em um base n')
+    print('3 - Multiplicação em uma base n')
+    print('OBS: Bases disponíveis vão de 2 até 26 (P)')
+    print('-------------------------------------------------------------------------------------------')
 
-    print('Preenchimento dos números')
-    base_origem = int(input('Digite a base do numero inserido: '))
+    operacao = int(input("Digite a operação escolhida: "))
 
-    while not bases.count(base_origem):
-        base_origem = int(input('A base que você inseriu não está contida no escopo deste programa! Tente novamente!\nAs bases disponíveis são de 2 a 26: '))
+    if operacao == 1:
+        print('\n-------------------------------------------------------------------------------------------\n')
+        print('Conversão de bases selecionada com sucesso\n')
+        print('-------------------------------------------------------------------------------------------\n')
 
-    base_origem_simbolos = list(simbolos[:base_origem])
+        print('Preenchimento dos números')
+        base_origem = int(input('Digite a base do numero inserido: '))
 
-    numero_base_1 = input('Digite o numero: ').upper()
+        while not bases.count(base_origem):
+            base_origem = int(input('A base que você inseriu não está contida no escopo deste programa! Tente novamente!\nAs bases disponíveis são de 2 a 26: '))
 
-    while not metodo_alternativo.valida(numero_base_1, base_origem):
-        numero_base_1 = input('O número escolhido está fora do escopo da base! Tente novamente!\n')
-        
-    base_destino =  int(input('Digite a base a qual você deseja converter: '))
+        base_origem_simbolos = list(simbolos[:base_origem])
 
-    while not bases.count(base_destino):
-        base_destino = int(input('A base que você inseriu não está contida no escopo deste programa! Tente novamente!\nAs bases disponíveis são de 2 a 26: '))
+        numero_base_1 = input('Digite o numero: ').upper()
 
-    if base_origem == base_destino:
-        print(f'As bases são iguais, assim, o número será o mesmo: {numero_base_1}')
-        exit()
-    
-    print('\n-------------------------------------------------------------------------------------------\n')
-    print('Aplicando o algoritmo...\n')
-    print('-------------------------------------------------------------------------------------------\n')
-    
-    n = 0 
-    numero_em_polinomio = ''
-
-    for i in range(len(numero_base_1)):
-        if len(numero_base_1) - (i+1):
-            numero_em_polinomio += f'{numero_base_1[i]}*{base_origem}**{len(numero_base_1) - (i+1)} + '
+        while not metodo_alternativo.valida(numero_base_1, base_origem):
+            numero_base_1 = input('O número escolhido está fora do escopo da base! Tente novamente!\n')
             
-        else:
-            numero_em_polinomio += f'{numero_base_1[i]}*{base_origem}**{len(numero_base_1) - (i+1)}'
-        n += 1
+        base_destino =  int(input('Digite a base a qual você deseja converter: '))
 
-    print(f'Escrevendo o numero na base {base_origem} como polinômio: {numero_em_polinomio}\n')
+        while not bases.count(base_destino):
+            base_destino = int(input('A base que você inseriu não está contida no escopo deste programa! Tente novamente!\nAs bases disponíveis são de 2 a 26: '))
 
-    print('-------------------------------------------------------------------------------------------\n')
-
-    print(f'Transformando cada termo de {numero_em_polinomio} para a base de destino {base_destino}\n')
-
-    print('-------------------------------------------------------------------------------------------\n')
-
-    print(f"Número convertido após sucessivas somas feitas nas base {base_destino} será:")
-    metodo_alternativo.converte(numero_base_1, base_origem, base_destino)
-    print('-------------------------------------------------------------------------------------------\n')
-    
-elif operacao == 2:
-    print('-------------------------------------------------------------------------------------------\n')
-    print('Operação de soma selecionada com sucesso\n')
-    print('-------------------------------------------------------------------------------------------\n')
-    base = int(input('Digite a base: '))
-    n1 = input('Digite o numero 01: ')
-
-    while not metodo_alternativo.valida(n1, base):
-        n1 = input(f'O número escolhido está fora do escopo da base {base}! Tente novamente!\n')
-
-    n2 = input('Digite o numero 02: ')
-    while not metodo_alternativo.valida(n1, base):
-        n1 = input(f'O número escolhido está fora do escopo da base {base}! Tente novamente!\n')
-
-    
-    print('-------------------------------------------------------------------------------------------\n')
-    print(f"Resultado obtido após a aplicação de somas na base {base} será: ")
-    # print(base_n.soma_em_base_n(n1, n2, base))
-    print(metodo_alternativo.soma_em_base_n(n1, n2, base) + f' (base {base})')
-    print('-------------------------------------------------------------------------------------------\n')
-    
-
-elif operacao == 3:
-    print('-------------------------------------------------------------------------------------------\n')
-    print('Operação de multiplicação selecionada com sucesso\n')
-    print('-------------------------------------------------------------------------------------------\n')
-    base = int(input('Digite a base: '))
-    
-    n1 = input('Digite o numero 01: ')
-    while not metodo_alternativo.valida(n1, base):
-        n1 = input(f'O número escolhido está fora do escopo da base {base}! Tente novamente!\n')
+        if base_origem == base_destino:
+            print(f'As bases são iguais, assim, o número será o mesmo: {numero_base_1}')
+            exit()
         
-    n2 = input('Digite o numero 02: ')
-    while not metodo_alternativo.valida(n2, base):
-        n2 = input(f'O número escolhido está fora do escopo da base {base}! Tente novamente!\n')
+        print('\n-------------------------------------------------------------------------------------------\n')
+        print('Aplicando o algoritmo...\n')
+        print('-------------------------------------------------------------------------------------------\n')
+        
+        n = 0 
+        numero_em_polinomio = ''
+
+        for i in range(len(numero_base_1)):
+            if len(numero_base_1) - (i+1):
+                numero_em_polinomio += f'{numero_base_1[i]}*{base_origem}**{len(numero_base_1) - (i+1)} + '
+                
+            else:
+                numero_em_polinomio += f'{numero_base_1[i]}*{base_origem}**{len(numero_base_1) - (i+1)}'
+            n += 1
+
+        print(f'Escrevendo o numero na base {base_origem} como polinômio: {numero_em_polinomio}\n')
+
+        print('-------------------------------------------------------------------------------------------\n')
+
+        print(f'Transformando cada termo de {numero_em_polinomio} para a base de destino {base_destino}\n')
+
+        print('-------------------------------------------------------------------------------------------\n')
+
+        print(f"Número convertido após sucessivas somas feitas nas base {base_destino} será:")
+        metodo_alternativo.converte(numero_base_1, base_origem, base_destino)
+        print('-------------------------------------------------------------------------------------------\n')
+        
+    elif operacao == 2:
+        print('-------------------------------------------------------------------------------------------\n')
+        print('Operação de soma selecionada com sucesso\n')
+        print('-------------------------------------------------------------------------------------------\n')
+        base = int(input('Digite a base: '))
+        while not bases.count(base):
+            base = int(input('A base que você inseriu não está contida no escopo deste programa! Tente novamente!\nAs bases disponíveis são de 2 a 26: '))
+        n1 = input('Digite o numero 01: ')
+
+        while not metodo_alternativo.valida(n1, base):
+            n1 = input(f'O número escolhido está fora do escopo da base {base}! Tente novamente!\n')
+
+        n2 = input('Digite o numero 02: ')
+        while not metodo_alternativo.valida(n1, base):
+            n1 = input(f'O número escolhido está fora do escopo da base {base}! Tente novamente!\n')
+
+        
+        print('-------------------------------------------------------------------------------------------\n')
+        print(f"Resultado obtido após a aplicação de somas na base {base} será: ")
+        # print(base_n.soma_em_base_n(n1, n2, base))
+        print(metodo_alternativo.soma_em_base_n(n1, n2, base) + f' (base {base})')
+        print('-------------------------------------------------------------------------------------------\n')
+        
+
+    elif operacao == 3:
+        print('-------------------------------------------------------------------------------------------\n')
+        print('Operação de multiplicação selecionada com sucesso\n')
+        print('-------------------------------------------------------------------------------------------\n')
+        
+        base = int(input('Digite a base: '))
+        while not bases.count(base):
+            base = int(input('A base que você inseriu não está contida no escopo deste programa! Tente novamente!\nAs bases disponíveis são de 2 a 26: '))
+        
+        n1 = input('Digite o numero 01: ')
+        while not metodo_alternativo.valida(n1, base):
+            n1 = input(f'O número escolhido está fora do escopo da base {base}! Tente novamente!\n')
+            
+        n2 = input('Digite o numero 02: ')
+        while not metodo_alternativo.valida(n2, base):
+            n2 = input(f'O número escolhido está fora do escopo da base {base}! Tente novamente!\n')
 
 
-    # print(base_n.multiplica_em_base_n(n1, n2, base) + f' (base {base})')
-    print(metodo_alternativo.multiplica_em_base_n(n1, n2, base) + f' (base {base})')
-    print('-------------------------------------------------------------------------------------------\n')
+        # print(base_n.multiplica_em_base_n(n1, n2, base) + f' (base {base})')
+        print(metodo_alternativo.multiplica_em_base_n(n1, n2, base) + f' (base {base})')
+        print('-------------------------------------------------------------------------------------------\n')
+
+    continua = input('Se deseja continuar a utilizar digite S ou s, caso contrário, o programa será fechado.\n')
